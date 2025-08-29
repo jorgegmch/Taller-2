3cd49f777929b5e3f1041b90607fb886553ff6fe
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2025-08-29 02:07:47 -0500

    second commit

--- ejerciciocajero.py
+++ ejerciciocajero.py
@@ -2,14 +2,47 @@ import os
 
 os.system("cls")
 
-print("BIENVENIDO A SU CAJERO BANCARROTA S.A.")
-
 def mostrar_menu():
+    print("Menú de opciones\n")
     print("1. Consultar saldo")
     print("2. Depositar dinero")
     print("3. Retirar dinero")
     print("0. Salir")
 
 isActive=True
+saldo=100000
+
+
 while isActive:
-    os.system
+    try:
+        os.system("cls")
+        print("BIENVENIDO A SU CAJERO BANCARROTA S.A.\n")
+        mostrar_menu()
+        option=int(input("\nSeleccione una opcion: "))
+        print("-"*60)
+        match option:
+            case 1:
+                print(f"\nTu saldo actual es de: {saldo:,.2f}\n")
+                os.system("pause")
+            case 2:
+                deposito=int(input("\nIngrese el monto que desea depositar: "))
+                saldo=saldo+deposito
+                print(f"\nHas ingresado un monto total de: {deposito} \nTu saldo actual es de: {saldo:,.2f}\n")
+                os.system("pause")
+            case 3:
+                retiro=int(input("\nIngrese el monto que desea retirar: "))
+                if retiro>saldo:
+                    print("Saldo insuficiente. Intente con otra cantidad.")
+                else:
+                    saldo=saldo-retiro
+                    print(f"\nHas retirado un total de: {retiro:,.2f} \nTu saldo actual es de: {saldo:,.2f}\n")
+                    os.system("pause")
+            case 0:
+                print("\nMUCHAS GRACIAS POR USAR CAJEROS BANCARROTA S.A.\n")
+                isActive=False
+            case _:
+                print("Opción no valida. Vuelva a intentar.")
+                os.system("pause")
+    except ValueError:
+        print("\nError: Solo se permiten numeros.\n")
+        os.system("pause")
